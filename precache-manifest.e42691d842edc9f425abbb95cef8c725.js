5a109c0eed339ef54155fe860815fc737bc3e590
Ricardo Agra <ricardo@labtestanalyzer.com>
2019-04-07 15:09:14 +0100

    Updates

---
+++ precache-manifest.e42691d842edc9f425abbb95cef8c725.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d1b9009599586221ce9",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.49420e7e.js"
+  },
+  {
+    "revision": "3f5d0f542c087883e133",
+    "url": "/react-tic-tac-toe/static/js/main.3cded7fa.chunk.js"
+  },
+  {
+    "revision": "094cd32745cbffbe8b46",
+    "url": "/react-tic-tac-toe/static/js/2.3911b881.chunk.js"
+  },
+  {
+    "revision": "3f5d0f542c087883e133",
+    "url": "/react-tic-tac-toe/static/css/main.c8a41f49.chunk.css"
+  },
+  {
+    "revision": "076ece9724e04cef2eb805379b5fa68c",
+    "url": "/react-tic-tac-toe/index.html"
+  }
+];
